11288259b18b13f7807b4518214fe26c0dfb4e98
Space-wolf <952867436@qq.com>
2018-09-04 21:21:57 +0800

    1.0
    2018.9.4日初次提交

---
+++ src/components/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="login">
+    <div class="login-box">
+      <h2>登录</h2>
+      <form @submit.prevent='sendLogin' autocomplete="off">
+        <div><input placeholder="请输入用户名" type="text" name="user" ref="userInput" /></div>
+        <div><input placeholder="请输入密码" type="password" name="password" ref="pwdInput" /></div>
+        <div class="login-btn"><input type="submit" value="一键登入" /></div>
+      </form>
+      <div class="back-index">
+        <router-link  to="/">首页>>></router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data () {
+          return {}
+        },
+        methods: {
+          sendLogin () {
+              // 登录
+            let userName = this.$refs.userInput.value;
+            let pwd = this.$refs.pwdInput.value;
+            this.$local.save("miaov", {
+                login: true,
+                userName: userName,
+                pwd: pwd
+            })
+
+            let redirect = this.$route.query.redirect;
+
+            if(!redirect){
+                redirect = 'project'
+            }
+            this.$router.push({
+              path: '/'+redirect
+            })
+          }
+        }
+    }
+</script>
+<style>
+
+</style>
